1b8120935d6da1c32f447f97132acb5830442e80
Yoo-Hyuna <84444362+Yoo-Hyuna@users.noreply.github.com>
2025-05-09 11:42:15 +0900

    Title: 바이러스 백신, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250509/바이러스 백신/vaccine-for-virus.cpp
+++ 250509/바이러스 백신/vaccine-for-virus.cpp
@@ -6,137 +6,103 @@
 
 using namespace std;
 
-
-int maxxx;
-int minnn = INT_MAX;
-int n, m, dist;
-vector<vector<int>> map;
-vector<vector<int>> countt;
-vector<vector<bool>> visited;
-queue<int> distttt;
-
 struct node {
-    int x;
-    int y;
+    int x, y;
 };
 
-vector<node> hp;
+int n, m;
+vector<vector<int>> map;
+vector<node> hospitals;
+int minTime = INT_MAX;
 
-//바이러스 제거
-int dellll(string str) {
+// 바이러스를 제거하는 BFS
+int spreadVirus(vector<int>& selected) {
+    vector<vector<int>> time(n, vector<int>(n, 0));
+    vector<vector<bool>> visited(n, vector<bool>(n, false));
     queue<node> q;
 
-    for (int i = 0; i < m; i++) {
-        q.push(hp[str[i] - '0']);
-        visited[hp[str[i] - '0'].x][hp[str[i] - '0'].y] == true;
-
+    // 선택된 병원들을 큐에 넣고 방문 처리
+    for (int idx : selected) {
+        node start = hospitals[idx];
+        q.push(start);
+        visited[start.x][start.y] = true;
     }
 
-
-    int dx[] = { -1,0,1,0 };
-    int dy[] = { 0,1,0,-1 };
-
-    node temp;
-    int nx, ny, pre;
-    dist = 1;
+    int dx[] = { -1, 0, 1, 0 };
+    int dy[] = { 0, 1, 0, -1 };
 
     while (!q.empty()) {
-        temp = q.front();
-        if (visited[temp.x][temp.y] == true) continue;
-        else visited[temp.x][temp.y] = true;
-        q.pop();
-        for (int i = 0; i < 4;i++) {
-            //시계방향으로 검사
-            nx = temp.x + dx[i];
-            ny = temp.y + dy[i];
-            pre = countt[temp.x][temp.y];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue; //(2,5) 확인
-            // 0 바이러스, 1 벽, 2 병원
-            if (map[nx][ny] == 0 && visited[nx][ny] == false && countt[nx][ny]==0) {
-                countt[nx][ny] = pre+1;
-                q.push({ nx,ny });
-            } 
+        node curr = q.front(); q.pop();
+        for (int i = 0; i < 4; ++i) {
+            int nx = curr.x + dx[i];
+            int ny = curr.y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+            if (visited[nx][ny]) continue;
+            if (map[nx][ny] == 1) continue;
+
+            visited[nx][ny] = true;
+            if (map[nx][ny] == 0) {
+                time[nx][ny] = time[curr.x][curr.y] + 1;
+            } else { // 병원
+                time[nx][ny] = time[curr.x][curr.y];
+            }
+            q.push({ nx, ny });
         }
     }
 
-    ///dfs문 안에서는 최대 값을 찾고 main에서 결과 값 모임에서 최소값을 찾아서 출력
-    maxxx = -1;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (visited[i][j] == false && map[i][j] == 0) {
-                return -1;
-                break;
-            }
-            if (countt[i][j] >= maxxx) {
-                maxxx = countt[i][j];
+    int maxTime = 0;
+
+    // 모든 바이러스 제거 가능 여부 확인
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (map[i][j] == 0) {
+                if (!visited[i][j]) return -1; // 바이러스 못 없앰
+                maxTime = max(maxTime, time[i][j]);
             }
         }
     }
-    return maxxx;
+    return maxTime;
 }
 
-
-void dfs(int dep, int start, int hpCnt, string str) {
-    if (dep >= m) {
-        //경우의수를 인자로 입력, 거리 결과 받아옴
-        distttt.push(dellll(str));
-        //if (dist <= minnn) {
-            //minnn = dist;
-        //}
-
-        countt = vector<vector<int>>(n, vector<int>(n, 0));
-        visited = vector<vector<bool>>(n, vector<bool>(n, false));
+// 병원 조합 DFS
+void dfs(int depth, int start, vector<int>& selected) {
+    if (depth == m) {
+        int result = spreadVirus(selected);
+        if (result != -1) {
+            minTime = min(minTime, result);
+        }
         return;
     }
 
-    for (int i = start; i < hpCnt; i++) {
-        dfs(dep + 1, i + 1, hpCnt, str + to_string(i));
+    for (int i = start; i < hospitals.size(); ++i) {
+        selected.push_back(i);
+        dfs(depth + 1, i + 1, selected);
+        selected.pop_back();
     }
 }
 
-
 int main() {
-   // freopen_s(new FILE*, "input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int input;
-    int cnt = 1;
     cin >> n >> m;
-    map = vector<vector<int>>(n, vector<int>(n, 0));
-    countt = vector<vector<int>>(n, vector<int>(n, 0));
-    visited = vector<vector<bool>>(n, vector<bool>(n, false));
+    map = vector<vector<int>>(n, vector<int>(n));
 
-
-
-    // 0 바이러스, 1 벽, 2 병원
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> input;
-            map[i][j]= input;
-            if (input == 2)
-                hp.push_back({ i,j });  //전체 병원 목록이 들어감
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> map[i][j];
+            if (map[i][j] == 2) {
+                hospitals.push_back({ i, j });
+            }
         }
     }
 
+    vector<int> selected;
+    dfs(0, 0, selected);
 
-
-
-    //병원 조합 선택 bfs
-    dfs(0, 0, hp.size(),"");
-    //dfs(3, 0, hp.size(),"123");
-
-
-    //다 검사 후 최소값 출력. 다만, 모든 경우의 수가 -1이면 -1출력
-    bool allcasearenotmeaning = true;
-    while (!distttt.empty()) {
-        if (distttt.front() != -1) {
-            if (distttt.front() <= minnn) minnn = distttt.front();
-            allcasearenotmeaning = false;
-        }
-        distttt.pop();
-    }
-    if (allcasearenotmeaning == true) cout << -1;
-    else cout << minnn;
-
+    if (minTime == INT_MAX) cout << -1;
+    else cout << minTime;
 
     return 0;
 }
